87edd9ccebb13e7657bb306e573855a38abbed57
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-05-24 14:44:24 +0700

    routing

--- app/core/App.php
+++ app/core/App.php
@@ -4,6 +4,18 @@ class App
 {
 	public function __construct()
 	{
-		echo "Lanjutkan Mamang!";
+		$url = $this->parseUrl();
+		var_dump($url);
+	}
+
+	public function parseUrl()
+	{
+		if(isset($_GET['url']))
+		{
+			$url = rtrim($_GET['url'], '/');
+			$url = filter_var($url, FILTER_SANITIZE_URL);
+			$url = explode('/', $url);
+			return $url;
+		}
 	}
 }
