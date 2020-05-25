6e21307a3a6bfc1ff94ba146efe036ee0f3e6d56
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-05-25 12:05:12 +0700

    create mahasiswa's page - creating model for mahasiswa's data (app/models/Mahasiswa_model.php)

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -4,4 +4,13 @@
 			<h2>Daftar Mahasiswa</h2>
 		</div>
 	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<ul>
+				<?php foreach ($variable as $key => $value): ?>
+					
+				<?php endforeach ?>
+			</ul>
+		</div>
+	</div>
 </div>
