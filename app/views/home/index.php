08a8c51f29c7116fa1de05a48f6f100c953bb667
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-05-25 08:26:31 +0700

    use bootstrap - using bootstrap for interface design in hame page

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,4 +1,11 @@
-<h1>Selamat datang di website Saya!</h1>
-<?php foreach ($data['user'] as $user): ?>
-	<p>Hai, nama saya <?= $user['nama']; ?>.</p>	
-<?php endforeach ?>
+<div class="jumbotron">
+	<div class="container">
+		<h1 class="display-4">Selamat datang di website Saya!</h1>
+	  <?php foreach ($data['user'] as $user): ?>
+			<p class="lead">Hai, nama saya <?= $user['nama']; ?>.</p>
+		<?php endforeach ?>
+	  <hr class="my-4">
+	  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+	  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+	</div>
+</div>
