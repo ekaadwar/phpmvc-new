49492cbe266da203e33f51e449c9df7ead1d7c42
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-05-25 07:34:25 +0700

    Basic URL - Making basic url with code BASE_URL for http://localhost/php mvc/public/ address. This code contained in the file : app/config/config.php

--- app/config/config.php
+++ app/config/config.php
@@ -4,3 +4,5 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'phpmvc');
+
+define('BASIC_URL', 'http://localhost/phpmvc/public');
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -2,5 +2,6 @@
 <html>
 <head>
 	<title><?= $data['title']; ?></title>
+	
 </head>
 <body>
